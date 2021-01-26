33b1b637b6a2e6b78d47336602d02aaa74da4be7
arthursl12 <arthursoutolima@gmail.com>
2021-01-26 08:19:22 -0300

    Modificações iniciais, aceitar IPv4 e IPv6

--- cliente.py
+++ cliente.py
@@ -1,7 +1,51 @@
+#!/usr/bin/env python3
+
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 50000))
-s.sendall(b"Hello, world")
-data = s.recv(1024)
-s.close()
-print('Received', repr(data))
+import argparse
+import sys
+
+def logexit(err):
+    print(err)
+    sys.exit(1)
+    
+
+def validIPv4(addr):
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+    except socket.error:
+        return False
+    return True
+
+def validIPv6(addr):
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+    except socket.error:
+        return False
+    return True
+
+
+def main():
+    # Parse dos argumentos
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ip", help="IP do servidor {v4|v6}")
+    parser.add_argument("porta", help="Porta do servidor", type=int)
+    parser.add_argument("arquivo", help="Arquivo a ser enviado")
+    args = parser.parse_args()
+    
+    # Valida IPv4 ou IPv6 passado
+    if validIPv4(args.ip):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif validIPv6(args.ip):
+        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    else:
+       logexit("Protocolo desconhecido")
+       
+    # Conecta com o servidor
+    s.connect((args.ip, args.porta))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+    s.close()
+    print('Received', repr(data))
+
+if __name__ == "__main__":
+    main()
--- servidor.py
+++ servidor.py
@@ -1,45 +1,80 @@
-import select, socket, sys, queue
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setblocking(0)
-server.bind(('localhost', 50000))
-server.listen(5)
-inputs = [server]
-outputs = []
-message_queues = {}
+#!/usr/bin/env python3
 
-while inputs:
-    readable, writable, exceptional = select.select(
-        inputs, outputs, inputs)
-    for s in readable:
-        if s is server:
-            connection, client_address = s.accept()
-            connection.setblocking(0)
-            inputs.append(connection)
-            message_queues[connection] = queue.Queue()
-        else:
-            data = s.recv(1024)
-            if data:
-                message_queues[s].put(data)
-                if s not in outputs:
-                    outputs.append(s)
+import queue
+import select
+import socket
+import sys
+import argparse
+
+def logexit(err):
+    print(err)
+    sys.exit(1)
+
+def main():
+    # Parse dos argumentos
+    parser = argparse.ArgumentParser()
+    parser.add_argument("porta", help="Porta do servidor", type=int)
+    parser.add_argument("-v","--version", 
+                        help="Versão do IP do servidor", default="v4")
+    args = parser.parse_args()
+    
+    # Inicialização
+    if (args.version == "v4"):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif (args.version == "v6"):
+        server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    else:
+        logexit("Protocolo desconhecido")
+    print ("[log] Servidor iniciado")
+    server.setblocking(0)
+    server.bind(('', args.porta))
+    server.listen(5)
+    infoServer = server.getsockname()
+    print (f"[log] Aguardando conexões em {infoServer[0]}, {infoServer[1]}")
+
+    # Loop principal
+    inputs = [server]
+    outputs = []
+    message_queues = {}
+
+    while inputs:
+        readable, writable, exceptional = select.select(
+            inputs, outputs, inputs)
+        for s in readable:
+            if s is server:
+                connection, client_address = s.accept()
+                connection.setblocking(0)
+                inputs.append(connection)
+                message_queues[connection] = queue.Queue()
+                print ('Got connection from', client_address) 
             else:
-                if s in outputs:
-                    outputs.remove(s)
-                inputs.remove(s)
-                s.close()
-                del message_queues[s]
+                data = s.recv(1024)
+                if data:
+                    message_queues[s].put(data)
+                    if s not in outputs:
+                        outputs.append(s)
+                else:
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
+                    del message_queues[s]
 
-    for s in writable:
-        try:
-            next_msg = message_queues[s].get_nowait()
-        except queue.Empty:
-            outputs.remove(s)
-        else:
-            s.send(next_msg)
+        for s in writable:
+            try:
+                next_msg = message_queues[s].get_nowait()
+            except queue.Empty:
+                outputs.remove(s)
+            else:
+                s.send(next_msg)
 
-    for s in exceptional:
-        inputs.remove(s)
-        if s in outputs:
-            outputs.remove(s)
-        s.close()
-        del message_queues[s]
+        for s in exceptional:
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+            del message_queues[s]
+
+
+if __name__ == "__main__":
+    main()
