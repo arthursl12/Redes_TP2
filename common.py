b3d72bd6450cd413f93570cffd238cb50f7ac41d
arthursl12 <arthursoutolima@gmail.com>
2021-01-26 12:11:30 -0300

    Mensagens info, hello e connection

--- common.py
+++ common.py
@@ -9,6 +9,7 @@ Fim         5
 FILE        6
 ACK         7
 '''
+MAX_FILENAME_SIZE = 15
 
 def msgId(b_msg):
     '''
@@ -18,9 +19,13 @@ def msgId(b_msg):
     Isso é representado pelos dois primeiros bytes da mensagem.
     O código correspondente da cada mensagem está na especificação
     '''
-    pass
+    assert type(b_msg) == bytearray
+    assert len(b_msg) >= 2
+    
+    id = b_msg[:2]
+    return int.from_bytes(id, "big")  
 
-def hello():
+def hello_encode():
     """
     Cria uma mensagem do tipo Hello: apenas 2 bytes, representando o 1
     """
@@ -29,8 +34,7 @@ def hello():
     ba.extend(i.to_bytes(length=2, byteorder='big'))
     return ba
 
-
-def connection(porta):
+def connection_encode(porta):
     """
     Cria uma mensagem do tipo Connection: 
         2 bytes: representando o 2
@@ -43,3 +47,70 @@ def connection(porta):
     ba.extend(i.to_bytes(length=2, byteorder='big'))
     ba.extend(porta.to_bytes(length=4, byteorder='big'))
     return ba
+
+def hello_decode(msg):
+    """
+    Decodifica uma mensagem em binário do tipo Hello.
+    Verifica seu tamanho e id, levanta uma exceção se houver algo errado
+    """
+    assert msgId(msg) == 1
+    assert len(msg) == 2
+
+def connection_decode(msg):
+    """
+    Decodifica uma mensagem em binário do tipo Connection, ou seja retorna o 
+    inteiro referente à porta UDP na mensagem (4 bytes)
+    """
+    assert msgId(msg) == 2
+    assert len(msg) == 6
+    
+    b_porta = msg[2:6]
+    return int.from_bytes(b_porta, "big")  
+
+def info_file_encode(nome_arq, size):
+    """
+    Cria uma mensagem do tipo Info File: 
+        2 bytes : representando o 3
+        15 bytes: nome do arquivo, incluindo extensão (string)
+        8 bytes : tamanho do arquivo, em bytes (número inteiro)
+    Nome com mais de 15 bytes levanta uma exceção
+    """
+    assert size >= 0
+    
+    # Id da mensagem
+    ba = bytearray()
+    i = 3
+    ba.extend(i.to_bytes(length=2, byteorder='big'))
+    
+    # Nome do arquivo (máximo 15 bytes)
+    b_str = nome_arq.encode("ascii")
+    print (len(b_str))
+    
+    # Completa com zeros até 15 bytes
+    # Se o nome tiver mais de 15, levantará uma exceção
+    filler = bytearray(MAX_FILENAME_SIZE - len(b_str))
+    ba.extend(filler)
+    ba.extend(b_str)
+    
+    # Coloca o tamanho do arquivo
+    ba.extend(size.to_bytes(length=8, byteorder='big'))
+    return ba
+
+def info_file_decode(msg):
+    """
+    Decodifica uma mensagem em binário do tipo Info File.
+    Retorna 
+        - O nome do arquivo (string)
+        - O seu tamanho em bytes (inteiro)
+    """
+    assert msgId(msg) == 3
+    assert len(msg) == 25
+    
+    
+    b_nome = msg[2:17]
+    b_size = msg[17:25]
+    
+    nome = b_nome.decode("ascii")
+    nome = nome.strip('\x00')       # Remove possíveis zeros à esquerda 
+    tam = int.from_bytes(b_size, "big")
+    return (nome, tam) 
--- test_common.py
+++ test_common.py
@@ -3,31 +3,102 @@ import binascii
 import common
 
 class Test01:
-    # def test_msgId(self):
-    #     ba = bytearray()
-    #     i = 332005
-    #     ba.extend(i.to_bytes(length=5, byteorder='big'))
-    #     msg1 = bytearray(2)
-    #     print (binascii.hexlify(ba,sep=","))
-    #     print (len(ba))
-    #     print (binascii.hexlify(ba[:3],sep=","))
-        
-    #     assert common.msgId(ba) == 1
-        
+    def test_msgId(self):
+        assert common.msgId(common.hello_encode()) == 1
+        assert common.msgId(common.connection_encode(40532)) == 2
+
+class TestHello:
     def test_hello_msg(self):
         ba = bytearray()
         i = 1
         ba.extend(i.to_bytes(length=2, byteorder='big'))
-        assert ba == common.hello()
+        assert ba == common.hello_encode()
     
+    def test_hello_decode(self):
+        common.hello_decode(common.hello_encode())
+        with pytest.raises(Exception) as e_info:
+            ba = bytearray()
+            i = 80
+            ba.extend(i.to_bytes(length=2, byteorder='big'))
+            common.hello_decode(ba)
+    
+class TestConnection:
     def test_connection_msg(self):
         ba = bytearray()
         i = 2
         ba.extend(i.to_bytes(length=2, byteorder='big'))
         i = 40214
         ba.extend(i.to_bytes(length=4, byteorder='big'))
-        assert ba == common.connection(40214)
+        assert ba == common.connection_encode(40214)
     
     def test_connection_msg_error(self):
         with pytest.raises(Exception) as e_info:
-            common.connection(-20)
+            common.connection_encode(-20)
+    
+    def test_connection_decode(self):
+        msg = common.connection_encode(40214)
+        assert common.connection_decode(msg) == 40214
+
+class TestInfoFile:
+    def test_info_file_encode(self):
+        # Id da mensagem
+        ba = bytearray()
+        i = 3
+        ba.extend(i.to_bytes(length=2, byteorder='big'))
+        
+        # Nome do arquivo (máximo 15 bytes)
+        string = "teste1.txt"
+        b_str = string.encode("ascii")
+        print (len(b_str))
+        
+        # Completa com zeros até 15 bytes
+        filler = bytearray(common.MAX_FILENAME_SIZE - len(b_str))
+        ba.extend(filler)
+        ba.extend(b_str)
+        
+        # Coloca o tamanho do arquivo
+        tam = 135
+        ba.extend(tam.to_bytes(length=8, byteorder='big'))
+        
+        # print(len(ba))
+        # print(binascii.hexlify(ba,","))
+        assert ba == common.info_file_encode("teste1.txt",135)
+    
+    def test_info_file_encode_without_filler(self):
+        # Id da mensagem
+        ba = bytearray()
+        i = 3
+        ba.extend(i.to_bytes(length=2, byteorder='big'))
+        
+        # Nome do arquivo (máximo 15 bytes)
+        string = "nomemtlongo.txt"
+        b_str = string.encode("ascii")
+        print (len(b_str))
+        
+        # Completa com zeros até 15 bytes
+        # filler = bytearray(common.MAX_FILENAME_SIZE - len(b_str))
+        # ba.extend(filler)
+        ba.extend(b_str)
+        
+        # Coloca o tamanho do arquivo
+        tam = 2200
+        ba.extend(tam.to_bytes(length=8, byteorder='big'))
+                
+        # print(len(ba))
+        # print(binascii.hexlify(ba,","))
+        assert ba == common.info_file_encode("nomemtlongo.txt",2200)
+    
+    def test_info_file_encode_name_too_big(self):
+        with pytest.raises(Exception) as e_info:
+            common.info_file_encode("nomemtlongo1.txt",2200)
+    
+    def test_info_file_decode(self):
+        assert common.info_file_decode(
+                    common.info_file_encode("teste1.txt",135)
+                ) \
+                == ("teste1.txt",135)
+        assert common.info_file_decode(
+                    common.info_file_encode("nomemtlongo.txt",2200)
+                ) \
+                == ("nomemtlongo.txt",2200)
+        
